fbf542563dea5604dbd84c2fe6fdf35b375456d4
Sean Oyler <xrysen@gmail.com>
2021-02-26 19:29:20 -0800

    Adds ball movement and border checking

--- pyPong.py
+++ pyPong.py
@@ -33,6 +33,8 @@ ball.shape("square")
 ball.color("white")
 ball.penup()
 ball.goto(0, 0)
+ball.dx = 0.2
+ball.dy = 0.2
 
 
 def paddle_a_up():
@@ -40,17 +42,53 @@ def paddle_a_up():
     y += 20
     paddle_a.sety(y)
 
+
 def paddle_a_down():
-  y = paddle_a.ycor()
-  y -= 20
-  paddle_a.sety(y)
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
 
 
 # keyboard binding
 wn.listen()
 wn.onkeypress(paddle_a_up, "w")
 wn.onkeypress(paddle_a_down, "s")
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
 
 # main game loop
 while(True):
     wn.update()
+
+    # Move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    # Border checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+
+    if ball.xcor() > 390:
+        ball.setx(390)
+        ball.dx *= -1
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+
+    if ball.xcor() < -390:
+        ball.setx(-390)
+        ball.dx *= -1
