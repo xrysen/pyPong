03bf411464740a13a3cea486f0cc90885eec50db
Sean Oyler <xrysen@gmail.com>
2021-02-26 19:53:15 -0800

    adds scoring

--- pyPong.py
+++ pyPong.py
@@ -6,6 +6,9 @@ wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
+score_a = 0
+score_b = 0
+
 # Paddle A
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
@@ -43,7 +46,7 @@ pen.color("white")
 pen.penup()
 pen.hideturtle()
 pen.goto(0, 260)
-pen.write("Player 1: 0  Player 2: 0", align="center", font=("Courier", 24, "normal"))
+pen.write(f"Player 1: {score_a}  Player 2: 0", align="center", font=("Courier", 24, "normal"))
 
 def paddle_a_up():
     y = paddle_a.ycor()
@@ -92,6 +95,9 @@ while(True):
     if ball.xcor() > 390:
         ball.goto(0, 0)
         ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write(f"Player 1: {score_a}  Player 2: {score_b}", align="center", font=("Courier", 24, "normal"))
 
     if ball.ycor() < -290:
         ball.sety(-290)
@@ -100,6 +106,9 @@ while(True):
     if ball.xcor() < -390:
         ball.goto(0, 0)
         ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write(f"Player 1: {score_a}  Player 2: {score_b}", align="center", font=("Courier", 24, "normal"))
 
     #Paddle collision
     if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40) and (ball.ycor() > paddle_b.ycor() - 40):
