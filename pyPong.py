c5f688b34e20bb77d9a030739df8d8acaf4b3177
Sean Oyler <xrysen@gmail.com>
2021-02-26 18:43:24 -0800

    Stretches left paddle

--- pyPong.py
+++ pyPong.py
@@ -11,8 +11,10 @@ paddle_a = turtle.Turtle()
 paddle_a.speed(0)
 paddle_a.shape("square")
 paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
 paddle_a.penup()
 paddle_a.goto(-350, 0)
+
 
 # Paddle B
 
