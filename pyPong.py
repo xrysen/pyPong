eab29a24a6139480fc8df4413f0ecdbfe31dc790
Sean Oyler <xrysen@gmail.com>
2021-02-26 18:43:59 -0800

    Sets up right paddle

--- pyPong.py
+++ pyPong.py
@@ -17,6 +17,14 @@ paddle_a.goto(-350, 0)
 
 
 # Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
 
 # Ball
 
