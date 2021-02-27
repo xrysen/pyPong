a0d0c532ee5f85fb738f2740a59aa8d781dd057c
Sean Oyler <xrysen@gmail.com>
2021-02-26 18:42:05 -0800

    Sets up left paddle

--- pyPong.py
+++ pyPong.py
@@ -1,7 +1,24 @@
 import turtle;
 
-wn = turtle.Screen
+wn = turtle.Screen()
 wn.title("pyPong")
 wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
+
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+# Paddle B
+
+# Ball
+
+#main game loop
+
+while(True):
+  wn.update()
